b1502958ad0d411118535ac48c85c06740682bca
Drew Zemke <drew.zemke@gmail.com>
2023-12-01 20:04:32 -0800

    day 1 (second)

--- aoc-2023-01/src/main.rs
+++ aoc-2023-01/src/main.rs
@@ -4,6 +4,11 @@ use std::{
 };
 
 fn main() {
+    first_puzzle();
+    second_puzzle();
+}
+
+fn first_puzzle() {
     let file = File::open("input").unwrap();
     let read = BufReader::new(file);
 
@@ -22,5 +27,74 @@ fn main() {
         sum += line_value;
     }
 
-    println!("{}", sum);
+    println!("first puzzle: {}", sum);
+}
+
+fn second_puzzle() {
+    let file = File::open("input").unwrap();
+    let read = BufReader::new(file);
+
+    let mut sum: u32 = 0;
+
+    for line in read.lines().map(Result::unwrap) {
+        let digits = find_digits(&line);
+        let first_digit = digits.first().unwrap();
+        let last_digit = digits.last().unwrap();
+
+        let line_value = first_digit * 10 + last_digit;
+        sum += line_value;
+    }
+
+    println!("second puzzle: {}", sum);
+}
+
+fn find_digits(s: &str) -> Vec<u32> {
+    let mut digits = vec![];
+    let mut haystack = s;
+    while !haystack.is_empty() {
+        if let (Some(digit), remainder) = parse_numerical_digit(haystack) {
+            digits.push(digit);
+            haystack = remainder;
+            continue;
+        }
+        if let (Some(digit), _) = parse_word_digit(haystack) {
+            digits.push(digit);
+            // can't do `haystack = remainder` here because it might skip over
+            // some matches (eg. "twone")
+            haystack = &haystack[1..];
+            continue;
+        } else {
+            haystack = &haystack[1..];
+        }
+    }
+
+    digits
+}
+
+fn parse_numerical_digit(s: &str) -> (Option<u32>, &str) {
+    s.chars()
+        .next()
+        .and_then(|c| c.to_digit(10))
+        .map_or((None, s), |d| (Some(d), &s[1..]))
+}
+
+const WORD_DIGITS: [(&str, u32); 9] = [
+    ("one", 1),
+    ("two", 2),
+    ("three", 3),
+    ("four", 4),
+    ("five", 5),
+    ("six", 6),
+    ("seven", 7),
+    ("eight", 8),
+    ("nine", 9),
+];
+
+fn parse_word_digit(s: &str) -> (Option<u32>, &str) {
+    for (word, digit) in WORD_DIGITS {
+        if let Some(remainder) = s.strip_prefix(word) {
+            return (Some(digit), remainder);
+        }
+    }
+    (None, s)
 }
