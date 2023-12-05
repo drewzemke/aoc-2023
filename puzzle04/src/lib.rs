74fcc096431ddbe5344e67ff50a3901e3becdef9
Drew Zemke <drew.zemke@gmail.com>
2023-12-05 12:41:59 -0800

    puzzle04: part a

---
+++ puzzle04/src/lib.rs
@@ -0,0 +1,2 @@
+pub mod puzzle04a;
+pub mod puzzle04b;
---
+++ puzzle04/src/puzzle04b.rs
@@ -0,0 +1,13 @@
+use common::puzzle::PuzzlePart;
+
+pub struct Puzzle04b {}
+
+impl PuzzlePart for Puzzle04b {
+    fn description() -> &'static str {
+        todo!()
+    }
+
+    fn solve(_input: &str) -> String {
+        todo!()
+    }
+}
